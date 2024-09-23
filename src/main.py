bd619a0fd97a376a1ef7081831ad2c24d1998f07
Arthur <arthurfsjesus@gmail.com>
2024-09-23 16:43:10 -0300

    removendo comentarios

--- src/main.py
+++ src/main.py
@@ -23,10 +23,3 @@ app.include_router(prefix="/api", router=pautaController.pauta)
 @app.get("/")
 def read_root():
     return {"message": "UnB-TV!"}
-
-# if __name__ == '__main__':
-#   port = 8080
-#   if (len(sys.argv) == 2):
-#     port = sys.argv[1]
-
-uvicorn.run('main:app', reload=True, port=int(port), host="0.0.0.0")
